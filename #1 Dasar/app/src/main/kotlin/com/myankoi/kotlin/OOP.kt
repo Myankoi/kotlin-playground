95edb7baeb2ac967e1c94de8bcb71b19ebf067f4
Myan <146714203+Myankoi@users.noreply.github.com>
2024-09-22 17:27:42 +0700

    make a package 📍

--- #1 Dasar/app/src/main/kotlin/com/myankoi/kotlin/OOP.kt
+++ #1 Dasar/app/src/main/kotlin/com/myankoi/kotlin/OOP.kt
@@ -1,6 +1,11 @@
 package com.myankoi.kotlin
 
-open class Animal(name: String, weight: Double, age: Int, isCarnivore: Boolean) {
+import com.myankoi.oop.utils.sayHello
+import com.myankoi.oop.utils.pow
+import com.myankoi.oop.utils.factorial
+import com.myankoi.oop.utils.areaOfCircle
+
+abstract class Animal(name: String, weight: Double, age: Int, isCarnivore: Boolean) {
     protected val name: String
     protected val weight: Double
     protected val age: Int
@@ -14,10 +19,10 @@ open class Animal(name: String, weight: Double, age: Int, isCarnivore: Boolean) 
     }
 
     open fun eat() {
-        println("${name} is eating")
+        println("$name is eating")
     }
     open fun sleep() {
-        println("${name} is sleeping")
+        println("$name is sleeping")
     }
 }
 
@@ -33,17 +38,17 @@ class Cat(pName: String, pWeight: Double, pAge: Int, pIsCarnivore: Boolean, furC
         }
 
     fun playWithHuman() {
-        println("${name} is playing with human")
+        println("$name is playing with human")
     }
 
     override fun eat() {
         super.eat()
-        println("${name} is eating fish")
+        println("$name is eating fish")
     }
 
     override fun sleep() {
         super.sleep()
-        println("${name} is sleeping on the pillow")
+        println("$name is sleeping on the pillow")
     }
 }
 
@@ -53,4 +58,11 @@ fun main() {
     cat.playWithHuman()
     cat.eat()
     cat.sleep()
+
+    println("aw\n")
+
+    sayHello()
+    println(pow(3, 2))
+    println(factorial(5))
+    println(areaOfCircle(10.0))
 }
